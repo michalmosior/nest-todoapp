fe3703dd7caa4bea240c24dc66300a542fd0eb9a
michalmosior <michal.mosior@gmail.com>
2023-05-03 11:15:45 +0200

    add task controller, task module, task schema, task service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27019/nest-todoapp'), TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
